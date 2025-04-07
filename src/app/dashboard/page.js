4fdef01015fb5dadbbd5ede2ad2b7ed76f6521be
NavenRaam <navenraam07@gmail.com>
2025-04-07 15:42:00 +0530

    signin server added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
   return (
-    <div>
-      <h1 className="text-2xl font-semibold">Your content goes here</h1>
+    <div className="all-[unset]" >
+      <h1 className="">Your content goes here</h1>
     </div>
   );
 }
